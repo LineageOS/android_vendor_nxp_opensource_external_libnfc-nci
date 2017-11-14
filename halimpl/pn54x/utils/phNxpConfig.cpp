77f5e56399bc82e07feb59618b4fbfa7c49a2ea8
Rohit Rangwani <rrangwan@codeaurora.org>
2017-11-14 18:02:06 +0530

    NFC: Add dynamic config support for MSM8909
    Dynamic configuration support is added for MSM8909 platform to pick
    Nfc configuration file at runtime to support NQ3xx chipsets
    
    Change-Id: I5dbe7b9894a005e14f4a5d162b8cb562f2c12f44

--- halimpl/pn54x/utils/phNxpConfig.cpp
+++ halimpl/pn54x/utils/phNxpConfig.cpp
@@ -353,7 +353,6 @@ int CNfcConfig::getconfiguration_id (char * config_file)
             config_id = CONFIG_GENERIC;
             break;
         case TARGET_MSM8952:
-        case TARGET_MSM8909:
             config_id = QRD_TYPE_DEFAULT;
             strlcpy(config_file, config_name_qrd, MAX_DATA_CONFIG_PATH_LEN);
             break;
@@ -361,6 +360,7 @@ int CNfcConfig::getconfiguration_id (char * config_file)
         case TARGET_MSM8937:
         case TARGET_MSM8917:
         case TARGET_MSM8940:
+        case TARGET_MSM8909:
             if (nq2xx_present) {
                 // NQ210 or NQ220
                 config_id = QRD_TYPE_DEFAULT;
@@ -415,6 +415,7 @@ int CNfcConfig::getconfiguration_id (char * config_file)
         case TARGET_MSM8937:
         case TARGET_MSM8917:
         case TARGET_MSM8940:
+        case TARGET_MSM8909:
             if (nq2xx_present) {
                 // NQ210 or NQ220
                 config_id = MTP_TYPE_DEFAULT;
