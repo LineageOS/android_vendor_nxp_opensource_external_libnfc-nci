c27336aa14e8b44560730d4a6b89e14195f7400a
Gaurav Singhal <gsinghal@codeaurora.org>
2019-04-19 10:55:39 +0530

    NFC: Fix compiler errors
    Fix build error for 10.00.04 MW release.
    
    Change-Id: Id72b017c6bed94fc7cfab7d26e2f5ec068b17874

--- SN100x/extns/impl/hal_nxpnfc.h
+++ SN100x/extns/impl/hal_nxpnfc.h
@@ -88,7 +88,7 @@ typedef struct
 
 #if(NXP_EXTNS == TRUE)
 /*
- * nxp_nfc_config_t shall contain the respective flag value from the
+ * nfc_config_t for NQx shall contain the respective flag value from the
  * libnfc-nxp.conf
  */
 typedef struct {
@@ -172,7 +172,7 @@ typedef struct {
   uint8_t uicc_wired_prt_mask;
   uint8_t wired_mode_rf_field_enable;
   uint8_t aid_block_route;
-}nxp_nfc_config_t;
+}nq_nfc_config_t;
 
 /*
  * outputData_t :ioctl has multiple commands/responses
@@ -189,6 +189,7 @@ typedef union{
     uint8_t             chipType;
 #if(NXP_EXTNS == TRUE)
     nxp_nfc_config_t    nxpConfigs;
+    nq_nfc_config_t    nqConfigs;
 #endif
 }outputData_t;
 
