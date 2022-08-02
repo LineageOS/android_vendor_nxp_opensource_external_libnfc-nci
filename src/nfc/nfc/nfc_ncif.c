485b230357a264e1429c3cb116365c724ca45c2d
Alisher Alikhodjaev <alisher@google.com>
2022-08-02 13:32:30 -0700

    The length of a packet should be non-zero
    Bug: 221856662
    Bug: 237079835
    Test: no functional changes, the build is ok
    Change-Id: I6defe4025c962ae7dde2e673e2bfcfc15785cc12
    (cherry picked from commit 396ac0e081ae67a1d743e0373257ec869692912c)
    Merged-In: I6defe4025c962ae7dde2e673e2bfcfc15785cc12
    [basilgello: Backport to LineageOS 14.1: adjust context]
    Signed-off-by: Vasyl Gello <vasek.gello@gmail.com>

--- src/nfc/nfc/nfc_ncif.c
+++ src/nfc/nfc/nfc_ncif.c
@@ -1903,12 +1903,12 @@ void nfc_ncif_proc_ee_discover_req(uint8_t* p, uint16_t plen) {
   tNFC_EE_DISCOVER_INFO* p_info;
   uint8_t u8;
 
-  NFC_TRACE_DEBUG2("nfc_ncif_proc_ee_discover_req %d len:%d", *p, plen);
-
   if (!plen) {
     android_errorWriteLog(0x534e4554, "221856662");
     return;
   }
+
+  NFC_TRACE_DEBUG2("nfc_ncif_proc_ee_discover_req %d len:%d", *p, plen);
 
   if (*p > NFC_MAX_EE_DISC_ENTRIES) {
     android_errorWriteLog(0x534e4554, "122361874");
